780155ccb8aeca121b2f40bc5f145c163f7af08b
Binh00001 <136829309+Binh00001@users.noreply.github.com>
2023-07-06 23:01:44 +0700

    fix UI

--- src/components/Layout/Footer/Footer.js
+++ src/components/Layout/Footer/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <div className="jc-evenly wrapper-footer">
             <a href="/">
-                <h1 className="logoFooter">Comics</h1>
+                <h1 className="logoFooter">Làng Truyện</h1>
             </a>
             <div className="copyright">
                 <p>Comics có rất nhiều comics hay.....</p>
